419cc89a31ee512a660483e46273cb063523a8bb
Carolini Paiva <caahpaiva@live.com>
2023-08-31 01:37:40 +0000

    first class

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask("Meu app")
+
+@app.route('/')
+def hello():
+    return "Hello World"
+
+
+@app.route('/novo')
+def novo():
+    return '<h1>Nova pagina</h1>'
