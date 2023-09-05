b31f282e883c674c74ab09383d83d3bb9495af99
Carolini Paiva <caahpaiva@live.com>
2023-09-05 01:07:50 +0000

    rota e HTML da pagina principal

--- app.py
+++ app.py
@@ -1,12 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask("Meu app")
+app = Flask("Meu App")
+
+
+posts = [
+    {
+        "titulo": "Minha primeira postagem", 
+        "texto": "teste"
+    },
+    {
+        "titulo": "Segundo Post",
+        "texto": "outro teste"
+    },
+
+]
 
 @app.route('/')
-def hello():
-    return "Hello World"
+def exibir_entradas():
+    entradas = posts # Mock das postagens
+    return render_template('exibir_entradas.html', entradas=entradas)
 
+@app.route('/layout')
+def layout():
+    return render_template('layout.html')
 
-@app.route('/novo')
-def novo():
-    return '<h1>Nova pagina</h1>'
+@app.route('/outro')
+def outro():
+    return render_template('outro_template.html')
