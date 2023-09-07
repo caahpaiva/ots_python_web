3ba84f9a7857c89891c74aaac30f39f8614b7e3a
Carolini Paiva <caahpaiva@live.com>
2023-09-07 01:30:17 +0000

    login completo e cmpleto com mock e pagina de detalhe

--- app.py
+++ app.py
@@ -1,40 +1,59 @@
 from flask import Flask, render_template, request, session, flash, redirect, url_for
 
-app = Flask("Meu App")
+app = Flask(__name__)
 app.config['SECRET_KEY'] = 'pudim'
 
 posts = [
-    {
+    {   
         "titulo": "Minha primeira postagem",
-        "texto": "teste"
+        "texto": ''' Ao contrário do que se acredita, Lorem Ipsum não é simplesmente um texto randômico. Com mais de 2000 anos, suas raízes podem ser encontradas em uma obra de literatura latina clássica datada de 45 AC. Richard McClintock, um professor de latim do Hampden-Sydney College na Virginia, pesquisou uma das mais obscuras palavras em latim, consectetur, oriunda de uma passagem de Lorem Ipsum, e, procurando por entre citações da palavra na literatura clássica, descobriu a sua indubitável origem. Lorem Ipsum vem das seções 1.10.32 e 1.10.33 do "de Finibus Bonorum et Malorum" (Os Extremos do Bem e do Mal), de Cícero, escrito em 45 AC. Este livro é um tratado de teoria da ética muito popular na época da Renascença. A primeira linha de Lorem Ipsum, "Lorem Ipsum dolor sit amet..." vem de uma linha na seção 1.10.32.
+O trecho padrão original de Lorem Ipsum, usado desde o século XVI, está reproduzido abaixo para os interessados. Seções 1.10.32 e 1.10.33 de "de Finibus Bonorum et Malorum" de Cicero também foram reproduzidas abaixo em sua forma exata original, acompanhada das versões para o inglês da tradução feita por H. Rackham em 1914. '''
     },
-    {
+    {   
         "titulo": "Segundo Post",
-        'texto': "outro teste"
-    }
-]
+        'texto': ''' Ao contrário do que se acredita, Lorem Ipsum não é simplesmente um texto randômico. Com mais de 2000 anos, suas raízes podem ser encontradas em uma obra de literatura latina clássica datada de 45 AC. Richard McClintock, um professor de latim do Hampden-Sydney College na Virginia, pesquisou uma das mais obscuras palavras em latim, consectetur, oriunda de uma passagem de Lorem Ipsum, e, procurando por entre citações da palavra na literatura clássica, descobriu a sua indubitável origem. Lorem Ipsum vem das seções 1.10.32 e 1.10.33 do "de Finibus Bonorum et Malorum" (Os Extremos do Bem e do Mal), de Cícero, escrito em 45 AC. Este livro é um tratado de teoria da ética muito popular na época da Renascença. A primeira linha de Lorem Ipsum, "Lorem Ipsum dolor sit amet..." vem de uma linha na seção 1.10.32.
+O trecho padrão original de Lorem Ipsum, usado desde o século XVI, está reproduzido abaixo para os interessados. Seções 1.10.32 e 1.10.33 de "de Finibus Bonorum et Malorum" de Cicero também foram reproduzidas abaixo em sua forma exata original, acompanhada das versões para o inglês da tradução feita por H. Rackham em 1914. '''
+}]
 
 @app.route('/')
 def exibir_entradas():
-    entradas = posts # Mock das postagens
+    entradas = posts [::-1] # Mock das postagens
     return render_template('exibir_entradas.html', entradas=entradas)
-
-
-@app.route('/layout')
-def layout():
-    return render_template('layout.html')
-
-@app.route('/outro')
-def outro():
-    return render_template('outro_template.html')
 
 @app.route('/login', methods=["GET", "POST"])
 def login():
     erro = None
     if request.method == "POST":
-        if request.form['username']  == "admin" and request.form['password'] == "admin":
+        if request.form['username'] == "admin" and request.form['password'] == "admin":
             session['logado'] = True
             flash("Login efetuado com sucesso!")
-            return render_template('exibir_entradas.html')
+            return redirect(url_for('exibir_entradas'))
         erro = "Usuário ou senha inválidos"        
     return render_template('login.html', erro=erro)
+
+
+@app.route('/logout')
+def logout ():
+    session.pop('logado')
+    flash('Logout efetuado com sucesso!')
+    return redirect(url_for('exibir_entradas'))
+
+@app.route('/inserir', methods=['POST'])
+def inserir_entradas():
+    if session['logado']:
+        novo_post = {
+            "titulo": request.form['titulo'],
+            "texto":  request.form['texto']
+        }
+        posts.append(novo_post)
+        flash('Post criado com sucesso!')
+    return redirect(url_for('exibir_entradas'))
+
+
+@app.route('/posts/<int:id>')
+def exibir_entrada(id):
+    try:
+         entrada = posts [id-1]
+         return render_template('exibir_entrada.html', entrada=entrada)
+    except Exception:
+        return abort(404)
